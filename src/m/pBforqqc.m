78462fb74665108f3514b441a6b05f5d358c4b45
4174122 <atwhatilold@gmail.com>
2025-07-31 06:55:04 +0300

    +pBforqqc

---
+++ src/m/pBforqqc.m
@@ -0,0 +1,12 @@
+	D usage Q
+usage
+	W "Export pB by qqc from qMS",!
+	W "Usage:",!
+	W " export to current device: d main(""qqc"")^"_$ZSOURCE,!
+	Q
+main(qqc)
+	N pBref,pB
+	S pBref=$G(^Q(1,153,qqc))
+	S pB=$G(^Q(1,"CpB",pBref))
+	W pB,!
+	Q
